4e31d7f9302aa20c28564c274e6970ce781492d6
Sattar Hummatli <settarxan@gmail.com>
2017-11-19 12:46:21 +0400

    add wishlist post and get

--- api/server.js
+++ api/server.js
@@ -22,11 +22,6 @@ var WishList = require('./model/wishlist')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get("/", function(req, res) {
-    console.log("Home")
-    res.send("Home")
-})
-
 app.post("/product", function(req, res) {
     var product = new Product()
     product.title = req.body.title
@@ -49,6 +44,50 @@ app.get("/product", function(req, res) {
             res.send(products)
         }
     })
+})
+
+app.post("/wishlist", function(req, res) {
+    var wishList = new WishList()
+    wishList.title = req.body.title
+    
+    wishList.save(function(err, savedWishList) {
+        if (err) {
+            res.status(500).send({error: "Could not save wishlist"})
+        } else {
+            res.send(savedWishList)
+        }
+    })
+})
+
+app.get("/wishlist", function(req, res) {
+    WishList.find({}).populate({path: 'products', model: 'Product'}).exec(function(err, wishLists) {
+        if (err) {
+            res.status(500).send({error: "Could not fetch wishlists"})
+        } else {
+            res.send(wishLists)
+        }
+    })
+})
+
+app.put("/wishlist/product/add", function(req, res) {
+    Product.findOne({_id: req.body.productId}, function(err, product) {
+        if (err) {
+           res.status(500).send({error: "Couldn't add item to a wishlist"})
+        } else {
+            WishList.update({_id: req.body.wishListId}, {$addToSet: {products: product._id}}, function(err, status) {
+                if (err) {
+                    res.status(500).send({error: "Couldn't add item to a wishlist"})
+                } else {
+                    res.send(status)
+                }
+            })
+        }
+    })
+})
+
+app.get("/", function(req, res) {
+    console.log("Home")
+    res.send("Home")
 })
 
 app.listen(3000, function() {
