344ae6eb53da9a1951b7b99f4ddd95d5e07b52a8
Sattar Hummatli <settarxan@gmail.com>
2017-11-24 13:58:55 +0400

    add listener to wishlist and add wishlist to it when product add wishlist button pressed

--- react-app/src/product/product.js
+++ react-app/src/product/product.js
@@ -1,15 +1,30 @@
 import React, {Component} from 'react'
 import './product.css'
+import DataService from '../services/data-service'
+
+let ds = new DataService()
 
 class Product extends Component {
+	
+	constructor(props) {
+		super(props)
+		
+		//Bind functions
+		this.onButtonClicked = this.onButtonClicked.bind(this)
+	}
+	
+	onButtonClicked = () => {
+		ds.addWishList(this.props.product)
+	}
+	
 	render() {
 		return (
 			<div className="card product"> 
-				<img className="card-img-top" src={this.props.imgUrl} alt="Product"></img>
+				<img className="card-img-top" src={this.props.product.imgUrl} alt="Product"></img>
 				<div className="card-block">
-					<h4 className="card-title">{this.props.title}</h4>
-					<p className="card-text">Price: ${this.props.price}</p>
-					<a href="#" className="btn btn-primary">Add To Wishlist</a>
+					<h4 className="card-title">{this.props.product.title}</h4>
+					<p className="card-text">Price: ${this.props.product.price}</p>
+					<a href="#" onClick={() => this.onButtonClicked()} className="btn btn-primary">Add To Wishlist</a>
 				</div>
 			</div>
 		)
